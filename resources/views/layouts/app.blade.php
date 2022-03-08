178c099637c90cba87240883774d5367d3a938f6
Enzo Aguiar <EnzoDarling@users.noreply.github.com>
2022-03-07 21:01:29 -0300

    Agregando Estilos a Inicio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{asset('css/app.css')}}" >
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
   <title>Podologia</title>
 </head>
 <body>
@@ -51,7 +52,7 @@
   <section class="container align-items-center py-4">
     @yield('content')
   </section>
-  
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
